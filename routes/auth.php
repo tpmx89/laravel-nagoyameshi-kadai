75583298c360a9d55f803d81f2a8fe0e32443528
tpmx89 <tpmx.d.728qyu@icloud.com>
2024-10-30 12:02:59 +0900

    管理者側の認証を実装

--- routes/auth.php
+++ routes/auth.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+Route::middleware(['guest', 'guest:admin'])->group(function () {
+Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
@@ -33,6 +34,13 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
+});
+
+Route::middleware('guest:admin')->group(function () {
+    Route::get('admin/login', [Admin\Auth\AuthenticatedSessionController::class, 'create'])
+                ->name('admin.login');
+
+    Route::post('admin/login', [Admin\Auth\AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
@@ -56,4 +64,9 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+});
+
+Route::middleware('auth:admin')->group(function () {
+    Route::post('admin/logout', [Admin\Auth\AuthenticatedSessionController::class, 'destroy'])
+                ->name('admin.logout');
 });
